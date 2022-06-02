018a79c24a3ed2a61167d3eeb09ed0dc33f1b205
Davide Frageri <davide.frag@gmail.com>
2022-06-02 18:54:30 +0200

    Fusion with backend + serverside get access token

--- pages/friends/index.js
+++ pages/friends/index.js
@@ -1,8 +1,33 @@
 import NavBar2 from "../../components/navbar/NavBar2";
 import FriendReqCard from "../../components/FriendReqCard";
 import FriendWallCard from "../../components/FriendWallCard";
+import { useEffect, useState } from "react";
+import { getAccessToken } from '@auth0/nextjs-auth0';
+import axios from "axios";
 
-export default function FriendsPage() {
+export default function FriendsPage({ accessToken }) {
+  const [friends, setFriends ] = useState([]);
+  const [requests, setRequests] = useState([]);
+  const [disable, setDisable ] = useState(false)
+  const url = (process.env.SECURE ? 'https://' : 'http://') + process.env.BACK_ENDPOINT;
+
+  useEffect(() => {
+    axios.get(url+'/api/friend',{
+      headers: {
+        Authorization: 'Bearer ' + accessToken
+      }
+    }).then((resp) => {
+      setFriends(resp.data);
+    })
+    axios.get(url+'/api/friend/request',{
+      headers: {
+        Authorization: 'Bearer ' + accessToken
+      }
+    }).then((resp) => {
+      setRequests(resp.data);
+    })
+    setDisable(false)
+  },[disable])
   return (
     <>
       <NavBar2 />
@@ -13,35 +38,17 @@ export default function FriendsPage() {
                         Amici
                     </div>
                     <div className="sticky top-0 p-1 w-full">
-                        <FriendWallCard name="Davide Frageri"/>
-                        <FriendWallCard name="Alberto Frageri"/>
-                        <FriendWallCard name="Tania Felice"/>
-                        <FriendWallCard name="Mario Rossi"/>
-                        <FriendWallCard name="Martina Frageri"/>
-                        <FriendWallCard name="Andrea Turnoli"/>
-                        
+                        {friends ? friends.map((f)=>{
+                          return (<FriendWallCard name={f.username}/>)
+                        }): (<h1>loading..</h1>)}
                     </div>
                 </aside>
                 <main role="main" className="w-full sm:w-2/3 md:w-3/4 pt-5 md:px-5 lg:px-5 ">
-                    <div className="grid gap-4 grid-cols-[repeat(auto-fit,_minmax(200px,1fr))]">
-                        <FriendReqCard favLang="c++" random="user1" />
-                        <FriendReqCard favLang="c++" random="user2" />
-                        <FriendReqCard favLang="c++" random="user3" />
-                        <FriendReqCard favLang="c++" random="user4" />
-                        <FriendReqCard favLang="c++" random="user5" />
-                        <FriendReqCard favLang="c++" random="user6" />
-                        <FriendReqCard favLang="c++" random="user1" />
-                        <FriendReqCard favLang="c++" random="user2" />
-                        <FriendReqCard favLang="c++" random="user3" />
-                        <FriendReqCard favLang="c++" random="user4" />
-                        <FriendReqCard favLang="c++" random="user5" />
-                        <FriendReqCard favLang="c++" random="user6" />
-                        <FriendReqCard favLang="c++" random="user1" />
-                        <FriendReqCard favLang="c++" random="user2" />
-                        <FriendReqCard favLang="c++" random="user3" />
-                        <FriendReqCard favLang="c++" random="user4" />
-                        <FriendReqCard favLang="c++" random="user5" />
-                        <FriendReqCard favLang="c++" random="user6" />
+                    <div className="grid gap-4 grid-cols-[repeat(auto-fill,_minmax(200px,1fr))]">
+                    {requests ? requests.map((r)=>{
+                          console.log(r._id)
+                          return (<FriendReqCard id={r._id} random={r.sender.username} accessToken={accessToken} setDisable={setDisable} />)
+                        }): (<h1>loading..</h1>)}
                     </div>
                 </main>
             </div>
@@ -50,4 +57,7 @@ export default function FriendsPage() {
   );
 }
 
-{/*  */}
+export async function getServerSideProps({ req, res }) {
+  const { accessToken } = await getAccessToken(req, res);
+  return { props: { accessToken } }
+}
