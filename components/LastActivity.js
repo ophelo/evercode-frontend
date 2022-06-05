b18c8c25515133fd2d6c7b04ceba396aebb88245
Davide Frageri <davide.frag@gmail.com>
2022-06-05 19:41:35 +0200

    Fix lint error + better refactor code

--- components/LastActivity.js
+++ components/LastActivity.js
@@ -1,13 +1,13 @@
 import useSWR from "swr";
 import { fetcherPost } from "../utils/fetcher";
 
-export default function LastActivity({ children, accessToken }) {
-  if (!accessToken) return (<>{children}</>);
-  
+export default function LastActivity({ children, accessToken }) { 
   const url = (process.env.SECURE ? 'https://' : 'http://') + process.env.BACK_ENDPOINT;
 
-  useSWR({url: url + '/api/user/setLastActivity', token: accessToken }, fetcherPost, {
-      refreshInterval: 10000
-    });
+  // SWR conditionally fetch
+  useSWR(accessToken ? {url: url + '/api/user/setLastActivity' , token: accessToken } : null, fetcherPost, {
+    refreshInterval: 10000
+  });
+
   return (<>{children}</>);
 }
